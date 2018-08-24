66f32296724fc1cdbe399a939f09cd1f142d8150
willmana <aleksi.willman@gmail.com>
2018-08-24 17:50:53 +0300

    final

--- application/sites/views.py
+++
@@ -1,7 +0,0 @@
-from application import app
-from flask import redirect, render_template
-
-
-@app.route("/sites/taulu/")
-def viikko_taulu():
-    return render_template("sites/taulu.html")
--- application/views.py
+++ application/views.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from application import app
 
-@app.route("/")
+@app.route("/tapahtumat")
 def index():
     return render_template("index.html")
+
+@app.route("/")
+def viikko_taulu():
+    return render_template("taulu.html")
